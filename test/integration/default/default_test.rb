ce9f47a8046c16d5bc42c504618a3aafca9278dc
Nick James <nick.james@eaglegenomics.com>
2019-03-08 17:53:54 +0000

    added 2 packages test install

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe command('Rscript -e \'packageVersion("ape")\'') do
+  its('exit_status') { should eq 0 }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command('Rscript -e \'packageVersion("optparse")\'') do
+  its('exit_status') { should eq 0 }
 end
